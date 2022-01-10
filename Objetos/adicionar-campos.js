7e4dd447df7527d0378325c998b6ffe6c7a27acb
Leonardo Zanoni Garcia <leo.zanoni@hotmail.com>
2022-01-10 20:28:05 -0300

    Create adicionar-campos

---
+++ Objetos/adicionar-campos.js
@@ -0,0 +1,16 @@
+const cliente = {
+    nome: "Andre",
+    idade: 36,
+    cpf: "12543652266",
+    email: "andre@email.com",
+}
+
+console.log(cliente);
+
+cliente.fone = "9264552626"
+
+console.log(cliente);
+
+cliente.fone = "85845554444"
+
+console.log(cliente);
